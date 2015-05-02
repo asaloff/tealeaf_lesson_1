0851e07ada2a6f6713c2dad4daf7074d66b1c7ed
Aaron Saloff <aaronsaloff@gmail.com>
2015-05-02 00:09:27 -0400

    First Blackjack

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -17,133 +17,122 @@ FULL_DECK = {"Ace of Hearts" => 1, "Ace of Spades" => 1, "Ace of Clubs" => 1, "A
 "King of Hearts" => 10, "King of Spades" => 10, "King of Clubs" => 10, "King of Diamonds" => 10 }
 
 def new_deck(deck)
-	FULL_DECK.keys.shuffle
+  FULL_DECK.keys.shuffle
 end
 
 def deal_card(deck, holder)
-	holder << deck.shift
+  holder << deck.shift
 end
 
 def card_total(holders_cards)
-	total = 0
-	holders_cards.each { |card| total += FULL_DECK[card] }
-	if holders_cards.any? { |card| card.include?("Ace") }
-		total += 10
-		total -= 10 if total > 21
-	end
-	total
+  total = 0
+  holders_cards.each { |card| total += FULL_DECK[card] }
+  if holders_cards.any? { |card| card.include?("Ace") }
+    total += 10
+    total -= 10 if total > 21
+  end
+  total
 end
 
 def hit_or_stay(choice)
-	puts "What would you like to do?"
-	choice = gets.chomp
-	until choice == 'stay' || choice == 'hit'
-		puts "You need to either enter: 'hit' or 'stay'"
-		choice = gets.chomp.downcase
-	end
-	choice
+  puts "What would you like to do?"
+  choice = gets.chomp
+  until choice == 'stay' || choice == 'hit'
+    puts "You need to either enter: 'hit' or 'stay'"
+    choice = gets.chomp.downcase
+  end
+  choice
 end
 
 def winner(winner, loser, type_of_win)
-	if type_of_win == 'bust'
-		print "#{loser.capitalize} busts, "
-	elsif type_of_win == 'blackjack'
-		print "#{winner.capitalize} has Blackjack!! "
-	elsif type_of_win == 'better_cards'
-		print "#{winner.capitalize}'s cards are better, "
-	else
-		print "Player and Dealer's cards are the same, "
-	end
+  if type_of_win == 'bust'
+    print "#{loser.capitalize} busts, "
+  elsif type_of_win == 'blackjack'
+    print "#{winner.capitalize} has Blackjack!! "
+  elsif type_of_win == 'better_cards'
+    print "#{winner.capitalize}'s cards are better, "
+  else
+    print "Player and Dealer's cards are the same, "
+  end
 
-	if winner == 'Dealer'
-		puts "Dealer Wins...Bummer"
-	elsif winner == 'Player'
-		puts "You Win!!"
-	else
-		puts "it's a Tie!!"
-	end
+  if winner == 'Dealer'
+    puts "Dealer Wins...Bummer"
+  elsif winner == 'Player'
+    puts "You Win!!"
+  else
+    puts "it's a Tie!!"
+  end
 end
 
 def end_game(in_play)
-	puts "Would you like to continue? ('y', 'n')"
-	in_play = gets.chomp
+  puts "Would you like to continue? ('y', 'n')"
+  in_play = gets.chomp
 end
 
 def in_play?(in_play)
-	if in_play != 'y'
-		true
-	else
-		false
-	end
+  if in_play != 'y'
+    true
+  else
+    false
+  end
 end
 
 deck = new_deck(deck)
 
 begin
 
-	in_play = 'y'
-	
-	if deck.length < 26
-		deck = new_deck(deck)
-	end
+  in_play = 'y'
+  
+  if deck.length < 26
+    deck = new_deck(deck)
+  end
 
-	player_cards = []
-	dealer_cards = []
-	
-	begin
-		deal_card(deck, player_cards)
-		deal_card(deck, dealer_cards)
-	end until player_cards.length == 2 && dealer_cards.length == 2
+  player_cards = []
+  dealer_cards = []
+  
+  begin
+    deal_card(deck, player_cards)
+    deal_card(deck, dealer_cards)
+  end until player_cards.length == 2 && dealer_cards.length == 2
 
-	puts "Dealer's Hand: #{dealer_cards}, total: #{card_total(dealer_cards)}"
-	puts "Player's Hand: #{player_cards}, total: #{card_total(player_cards)}"
+  puts "Dealer's Hand: #{dealer_cards}, total: #{card_total(dealer_cards)}"
+  puts "Player's Hand: #{player_cards}, total: #{card_total(player_cards)}"
 
-	choice = hit_or_stay(choice)
-	while choice == 'hit'
-		deal_card(deck, player_cards)
-		puts "You were dealt a #{player_cards.last}. Your hand: #{player_cards}, total: #{card_total(player_cards)}"
-		if card_total(player_cards) > 21
-			winner('Dealer', 'Player', 'bust')
-			in_play = end_game(in_play)
-			choice = nil
-		elsif card_total(player_cards) == 21
-			winner('Player', 'Dealer', 'blackjack')
-			in_play = end_game(in_play)
-			choice = nil
-		else
-			choice = hit_or_stay(choice)
-		end
-	end
-	
-		while card_total(dealer_cards) < 17 && in_play?(in_play)
-			deal_card(deck, dealer_cards)
-			puts "Dealer hits: Dealer's Current Hand: #{dealer_cards}, total: #{card_total(dealer_cards)}"
-		end
-
-		if card_total(dealer_cards) > 21 && in_play?(in_play)
-			winner('Player', 'Dealer', 'bust')
-			in_play = end_game(in_play)
-		elsif card_total(dealer_cards) == 21 && in_play?(in_play)
-			winner('Dealer', 'Player', 'blackjack')
-			in_play = end_game(in_play)
-		elsif card_total(player_cards) > card_total(dealer_cards) && in_play?(in_play)
-			winner('Player', 'Dealer', 'better_cards')
-			in_play = end_game(in_play)
-		elsif card_total(player_cards) < card_total(dealer_cards) && in_play?(in_play)
-			winner('Dealer', 'Player', 'better_cards')
-			in_play = end_game(in_play)
-		elsif card_total(player_cards) == card_total(dealer_cards) && in_play?(in_play)
-			winner('Tie', 'Tie', 'same_cards') 
-			in_play = end_game(in_play)
-		end
+  choice = hit_or_stay(choice)
+  while choice == 'hit'
+    deal_card(deck, player_cards)
+    puts "You were dealt a #{player_cards.last}. Your hand: #{player_cards}, total: #{card_total(player_cards)}"
+    if card_total(player_cards) > 21
+      winner('Dealer', 'Player', 'bust')
+      in_play = end_game(in_play)
+      choice = nil
+    elsif card_total(player_cards) == 21
+      winner('Player', 'Dealer', 'blackjack')
+      in_play = end_game(in_play)
+      choice = nil
+    else
+      choice = hit_or_stay(choice)
+    end
+  end
+  
+  while card_total(dealer_cards) < 17 && in_play?(in_play)
+    deal_card(deck, dealer_cards)
+    puts "Dealer hits: Dealer's Current Hand: #{dealer_cards}, total: #{card_total(dealer_cards)}"
+  end
+  if card_total(dealer_cards) > 21 && in_play?(in_play)
+    winner('Player', 'Dealer', 'bust')
+     in_play = end_game(in_play)
+  elsif card_total(dealer_cards) == 21 && in_play?(in_play)
+     winner('Dealer', 'Player', 'blackjack')
+    in_play = end_game(in_play)
+  elsif card_total(player_cards) > card_total(dealer_cards) && in_play?(in_play)
+    winner('Player', 'Dealer', 'better_cards')
+    in_play = end_game(in_play)
+  elsif card_total(player_cards) < card_total(dealer_cards) && in_play?(in_play)
+    winner('Dealer', 'Player', 'better_cards')
+    in_play = end_game(in_play)
+  elsif card_total(player_cards) == card_total(dealer_cards) && in_play?(in_play)
+    winner('Tie', 'Tie', 'same_cards') 
+    in_play = end_game(in_play)
+  end
 end until in_play != 'y'
-
-
-
-
-
-
-
-
-
-
