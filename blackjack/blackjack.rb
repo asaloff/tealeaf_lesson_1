a347e9828cce806894d660b91004b02720912c3c
Aaron Saloff <aaronsaloff@gmail.com>
2015-05-01 23:55:42 -0400

    First BlackJack

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -2,20 +2,19 @@
 
 require 'pry'
 
-FULL_DECK = {"Ace of Hearts" => [1, 11], "Ace of Spades" => [1, 11], "Ace of Clubs" => [1, 11], "Ace of Diamonds" => [1, 11], 
-	  				 "Two of Hearts" => 2, "Two of Spades" => 2, "Two of Clubs" => 2, "Two of Diamonds" => 2,
-	  				 "Three of Hearts" => 3, "Three of Spades" => 3, "Three of Clubs" => 3, "Three of Diamonds" => 3,
-	  				 "Four of Hearts" => 4, "Four of Spades" => 4, "Four of Clubs" => 4, "Four of Diamonds" => 4,
-	  				 "Five of Hearts" => 5, "Five of Spades" => 5, "Five of Clubs" => 5, "Five of Diamonds" => 5,
-	           "Six of Hearts" => 6, "Six of Spades" => 6, "Six of Clubs" => 6, "Six of Diamonds" => 6,
-	  		     "Seven of Hearts" => 7, "Seven of Spades" => 7, "Seven of Clubs" => 7, "Seven of Diamonds" => 7,
-	  			   "Eight of Hearts" => 8, "Eight of Spades" => 8, "Eight of Clubs" => 8, "Eight of Diamonds" => 8,
-	  			   "Nine of Hearts" => 9, "Nine of Spades" => 9, "Nine of Clubs" => 9, "Nine of Diamonds" => 9,
-	  			   "Ten of Hearts" => 10, "Ten of Spades" => 10, "Ten of Clubs" => 10, "Ten of Diamonds" => 10,
-	  				 "Jack of Hearts" => 10, "Jack of Spades" => 10, "Jack of Clubs" => 10, "Jack of Diamonds" => 10,
-	  				 "Queen of Hearts" => 10, "Queen of Spades" => 10, "Queen of Clubs" => 10, "Queen of Diamonds" => 10,
-	  				 "King of Hearts" => 10, "King of Spades" => 10, "King of Clubs" => 10, "King of Diamonds" => 10,
- 					  }
+FULL_DECK = {"Ace of Hearts" => 1, "Ace of Spades" => 1, "Ace of Clubs" => 1, "Ace of Diamonds" => 1, 
+"Two of Hearts" => 2, "Two of Spades" => 2, "Two of Clubs" => 2, "Two of Diamonds" => 2,
+"Three of Hearts" => 3, "Three of Spades" => 3, "Three of Clubs" => 3, "Three of Diamonds" => 3,
+"Four of Hearts" => 4, "Four of Spades" => 4, "Four of Clubs" => 4, "Four of Diamonds" => 4,
+"Five of Hearts" => 5, "Five of Spades" => 5, "Five of Clubs" => 5, "Five of Diamonds" => 5,
+"Six of Hearts" => 6, "Six of Spades" => 6, "Six of Clubs" => 6, "Six of Diamonds" => 6,
+"Seven of Hearts" => 7, "Seven of Spades" => 7, "Seven of Clubs" => 7, "Seven of Diamonds" => 7,
+"Eight of Hearts" => 8, "Eight of Spades" => 8, "Eight of Clubs" => 8, "Eight of Diamonds" => 8,
+"Nine of Hearts" => 9, "Nine of Spades" => 9, "Nine of Clubs" => 9, "Nine of Diamonds" => 9,
+"Ten of Hearts" => 10, "Ten of Spades" => 10, "Ten of Clubs" => 10, "Ten of Diamonds" => 10,
+"Jack of Hearts" => 10, "Jack of Spades" => 10, "Jack of Clubs" => 10, "Jack of Diamonds" => 10,
+"Queen of Hearts" => 10, "Queen of Spades" => 10, "Queen of Clubs" => 10, "Queen of Diamonds" => 10,
+"King of Hearts" => 10, "King of Spades" => 10, "King of Clubs" => 10, "King of Diamonds" => 10 }
 
 def new_deck(deck)
 	FULL_DECK.keys.shuffle
@@ -27,16 +26,12 @@ end
 
 def card_total(holders_cards)
 	total = 0
-	ace_total = [0, 0]
+	holders_cards.each { |card| total += FULL_DECK[card] }
 	if holders_cards.any? { |card| card.include?("Ace") }
-		holders_cards.each do |card| 
-			ace_total.each_with_index { |value, index| value + FULL_DECK[card][index] }
-		end	
-		return ace_total
-	else
-		holders_cards.each { |card| total = total + FULL_DECK[card] }
-		return total
+		total += 10
+		total -= 10 if total > 21
 	end
+	total
 end
 
 def hit_or_stay(choice)
@@ -76,9 +71,9 @@ end
 
 def in_play?(in_play)
 	if in_play != 'y'
-		false
-	else
 		true
+	else
+		false
 	end
 end
 
@@ -119,28 +114,28 @@ begin
 			choice = hit_or_stay(choice)
 		end
 	end
-		
-	while card_total(dealer_cards) < 17 && in_play?(in_play)
-		deal_card(deck, dealer_cards)
-		puts "Dealer hits: Dealer's Current Hand: #{dealer_cards}, total: #{card_total(dealer_cards)}"
-	end
+	
+		while card_total(dealer_cards) < 17 && in_play?(in_play)
+			deal_card(deck, dealer_cards)
+			puts "Dealer hits: Dealer's Current Hand: #{dealer_cards}, total: #{card_total(dealer_cards)}"
+		end
 
-	if card_total(dealer_cards) > 21 && in_play?(in_play)
-		winner('Player', 'Dealer', 'bust')
-		in_play = end_game(in_play)
-	elsif card_total(dealer_cards) == 21 && in_play?(in_play)
-		winner('Dealer', 'Player', 'blackjack')
-		in_play = end_game(in_play)
-	elsif card_total(player_cards) > card_total(dealer_cards) && in_play?(in_play)
-		winner('Player', 'Dealer', 'better_cards')
-		in_play = end_game(in_play)
-	elsif card_total(player_cards) < card_total(dealer_cards) && in_play?(in_play)
-		winner('Dealer', 'Player', 'better_cards')
-		in_play = end_game(in_play)
-	elsif card_total(player_cards) == card_total(dealer_cards) && in_play?(in_play)
-		winner('Tie', 'Tie', 'same_cards') 
-		in_play = end_game(in_play)
-	end
+		if card_total(dealer_cards) > 21 && in_play?(in_play)
+			winner('Player', 'Dealer', 'bust')
+			in_play = end_game(in_play)
+		elsif card_total(dealer_cards) == 21 && in_play?(in_play)
+			winner('Dealer', 'Player', 'blackjack')
+			in_play = end_game(in_play)
+		elsif card_total(player_cards) > card_total(dealer_cards) && in_play?(in_play)
+			winner('Player', 'Dealer', 'better_cards')
+			in_play = end_game(in_play)
+		elsif card_total(player_cards) < card_total(dealer_cards) && in_play?(in_play)
+			winner('Dealer', 'Player', 'better_cards')
+			in_play = end_game(in_play)
+		elsif card_total(player_cards) == card_total(dealer_cards) && in_play?(in_play)
+			winner('Tie', 'Tie', 'same_cards') 
+			in_play = end_game(in_play)
+		end
 end until in_play != 'y'
 
 
